c0769c56810001267b8561659db19801086b1ca4
Nch MOSFET <91242561+Nch-MOSFET@users.noreply.github.com>
2022-04-23 10:51:42 +0900

    Update main.ino

--- KeyBoard/Physical_Flick_Keyboard/Mega32U4/main/main.ino
+++ KeyBoard/Physical_Flick_Keyboard/Mega32U4/main/main.ino
@@ -1,9 +1,33 @@
-#include <Keyboard.h>
+//#include <Keyboard.h>
+#include <SPI.h>
+
+#define BaudRate 115200
+
+//実際の回路に合わせて要変更:
+#define MISO 12
+#define MOSI 11
+#define  CLK 13
+
+uint8_t    CS[  ] = {5, 6, 7};  //実際の回路に合わせて要変更:
+uint8_t  Buff[ 2];
+uint8_t  data[20];  //8, 8, 4bit:
+uint16_t ADCs;
+
+SPISettings settings(1000000, MSBFIRST, SPI_MODE0);
 
 void setup() {
-  Keyboard.begin();
+//  Keyboard.begin();
+  Serial.begin(BaudRate);
+  SPI.begin();
+
+  Serial.print("sizeof(CS) = ");
+  Serial.println(sizeof(CS));
+  for(uint8_t i = 0; i < sizeof(CS); i++){
+    pinMode     (CS[i], OUTPUT);
+    digitalWrite(CS[i],   HIGH);
+  }
 }
 
 void loop() {
-  
+  SPI.beginTransaction(settings);
 }
